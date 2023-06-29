a7f1624d96b06b22f404b069f006fd36b33d1fc0
estefano chacin <estefanochacin@wposs.com>
2023-06-29 10:12:48 -0500

    se implementa el listar y la creacion del usuario desde el front con js

--- SistemaVenta.AplicacionWeb/wwwroot/js/vistas/Usuario_index.js
+++ SistemaVenta.AplicacionWeb/wwwroot/js/vistas/Usuario_index.js
@@ -11,24 +11,54 @@
 let tablaData;
 
 $(document).ready(function () {
+    fetch("Usuario/ListaRoles")
+        .then(response => {
+            return response.ok ? response.json() : Promise.reject(response);
+        })
+        .then(data => {
+            if (data.length > 0) {
+                data.forEach(item => {
+                    $("#cboRol").append
+                        (
+                            $("<option>").val(item.idRol).text(item.descripcion)
+                        )
+                })
+            }
+        })
+
     tablaData = $('#tbdata').DataTable({
         responsive: true,
-         "ajax": {
-             "url": '',
-             "type": "GET",
-             "datatype": "json"
-         },
-         "columns": [
-             { "data": "clave" },
-             {
-                 "defaultContent": '<button class="btn btn-primary btn-editar btn-sm mr-2"><i class="fas fa-pencil-alt"></i></button>' +
-                     '<button class="btn btn-danger btn-eliminar btn-sm"><i class="fas fa-trash-alt"></i></button>',
-                 "orderable": false,
-                 "searchable": false,
-                 "width": "80px"
-             }
-         ],
-         order: [[0, "desc"]],
+        "ajax": {
+            "url": '/Usuario/Lista',
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "idUsuario", "visible": false, "searchable": false },
+            {
+                "data": "urlFoto", render: function (data) {
+                    return `<img style="height:60px" src=${data} class="rounded mx-auto d-block"/>`
+                }
+            },
+            { "data": "nombre" },
+            { "data": "correo" },
+            { "data": "telefono" },
+            { "data": "nombreRol" },
+            {
+                "data": "esActivo", render: function (data) {
+                    if (data == 1) { return '<span class="badge badge-info">Activo</span>' }
+                    else { return '<span class="badge badge-danger">No Activo</span>' }
+                }
+            },
+            {
+                "defaultContent": '<button class="btn btn-primary btn-editar btn-sm mr-2"><i class="fas fa-pencil-alt"></i></button>' +
+                    '<button class="btn btn-danger btn-eliminar btn-sm"><i class="fas fa-trash-alt"></i></button>',
+                "orderable": false,
+                "searchable": false,
+                "width": "80px"
+            }
+        ],
+        order: [[0, "desc"]],
         dom: "Bfrtip",
         buttons: [
             {
@@ -37,7 +67,7 @@ $(document).ready(function () {
                 title: '',
                 filename: 'Reporte Usuarios',
                 exportOptions: {
-                    columns: [1, 2]
+                    columns: [2, 3, 4, 5, 6]
                 }
             }, 'pageLength'
         ],
@@ -45,6 +75,81 @@ $(document).ready(function () {
             url: "https://cdn.datatables.net/plug-ins/1.11.5/i18n/es-ES.json"
         },
     });
+})
 
 
+
+
+
+function MostrarModal(modelo = MODELO_BASE) {
+    $("#txtId").val(modelo.idRol);
+    $("#txtNombre").val(modelo.nombre);
+    $("#txtCorreo").val(modelo.correo);
+    $("#txtTelefono").val(modelo.telefono);
+    $("#cboRol").val(modelo.idRol == 0 ? $("#cboRol option.first").val() : modelo.idRol);
+    $("#cboEstado").val(modelo.esActivo);
+    $("#txtFoto").val("");
+    $("#imgUsuario").attr("src", modelo.urlFoto)
+
+    $("#modalData").modal("show");
+}
+
+
+
+
+
+$("#btnNuevo").click(function () {
+    MostrarModal();
+})
+
+
+
+
+
+$("#btnGuardar").click(function () {
+    const inputs = $("input.input-validar").serializeArray();
+    const inputsSinValor = inputs.filter(item => item.value.trim() == "");
+
+    if (inputsSinValor.length > 0) {
+        const mensaje = `Debe completar el campo: ${inputsSinValor[0].name}`;
+        toastr.warning("", mensaje)
+        $(`input[name=${inputsSinValor[0].name}]`).focus();
+        return;
+    }
+
+    const modelo = structuredClone(MODELO_BASE);
+    modelo["idUsuario"] = parseInt($("#txtId").val());
+    modelo["nombre"] = $("#txtNombre").val();
+    modelo["correo"] = $("#txtCorreo").val();
+    modelo["telefono"] = $("#txtTelefono").val();
+    modelo["idRol"] = $("#cboRol").val();
+    modelo["esActivo"] = $("#cboEstado").val();
+
+    const inputFile = document.getElementById("txtFoto");
+    const formData = new FormData();
+
+    formData.append("foto", inputFile.files[0]);
+    formData.append("modelo", JSON.stringify(modelo));
+
+    $("#modalData").find("div.modal-content").LoadingOverlay("show");
+
+    if (modelo.idUsuario == 0) {
+        fetch("/Usuario/Crear", {
+            method: "POST",
+            body: formData
+        })
+            .then(response => {
+                $("#modalData").find("div.modal-content").LoadingOverlay("hide");
+                return response.ok ? response.json() : Promise.reject(response);
+            })
+            .then(data => {
+                if (data.estado) {
+                    tablaData.row.add(data.objecto).draw(false)
+                    $("#modalData").modal("hide");
+                    swal("Listo!", "El usuario fue creado", "success")
+                } else {
+                    swal("Los sentimos", data.mensaje, "error")
+                }
+            })
+    }
 })
